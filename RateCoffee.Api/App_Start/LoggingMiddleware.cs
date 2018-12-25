dc14e8786c9a179cfbaecfada8f07c1b8fe74800
LaurenceKrall <laurencekrall@gmail.com>
2018-12-25 17:20:07 +0000

    add start of logging middleware

---
+++ RateCoffee.Api/App_Start/LoggingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace RateCoffee.Api.App_Start
+{
+    public class LoggingMiddleware : OwinMiddleware
+    {
+        public LoggingMiddleware(OwinMiddleware next)
+            : base(next)
+        {
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            //handle request logging
+            var request = context.Request;
+
+            await Next.Invoke(context);
+
+            var response = context.Response;
+            //handle response logging
+        }
+
+    }
+}
---
+++ RateCoffee.Api/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using RateCoffee.Api.App_Start;
+
+[assembly: OwinStartup(typeof(RateCoffee.Api.Startup))]
+
+namespace RateCoffee.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+            app.Use<LoggingMiddleware>();
+            app.UseWebApi(config);
+        }
+    }
+}
